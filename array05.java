d3ceeef6ca7ba2540e5ad4842befc7c1fb04335f
SIDDHARTH SINHA <siddharthsinha670@gmail.com>
2026-05-25 22:26:00 +0530

    Add name class with main method to print a message

--- array05.java
+++ array05.java
@@ -1,13 +1,11 @@
 //User se n numbers lo aur print karo.
-
-import java.util.Scanner;
 class array05
 {
     public static void main(String args[])
     {
         int a[]=new int [5];
         System.out.println("Enter the Array of the Element");
-        a[int i]=sc.nextInt();
+        
 
     }
 }
