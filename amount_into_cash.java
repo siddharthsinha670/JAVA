3ff074653ef9953ab95ac230ab4dfb09ada778eb
SIDDHARTH SINHA <siddharthsinha670@gmail.com>
2026-06-01 22:15:09 +0530

    Implement denomination breakdown in amount_into_cash
    Read an integer amount from stdin and compute counts for currency denominations, then print counts for 500, 200, 100, 50, 20, 10, 5, 2 and 1. Adds the compiled amount_into_cash.class and updates amount_into_cash.java to perform the calculation and close the scanner. Note: there are apparent typos in the 50/20 calculations (n50 uses division by 20 and n20 is computed twice); those should be corrected in a follow-up.

--- amount_into_cash.java
+++ amount_into_cash.java
@@ -5,6 +5,46 @@ import java.util.Scanner;
 class amount_into_cash {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the amount: ");
+        int amount = sc.nextInt();
+
+        int n500 = amount / 500;
+        amount = amount % 500;
+
+        int n200 = amount / 200;
+        amount = amount % 200;
+
+        int n100 = amount / 100;
+        amount = amount % 100;
+
+        int n50 = amount / 20;
+        amount = amount % 20;
+
+        int n10 = amount / 10;
+        amount = amount % 10;
+
+        int n20 = amount / 20;
+        amount = amount % 20;
+
+        int n5 = amount / 5;
+        amount = amount % 5;
+
+        int n2 = amount / 2;
+        amount = amount % 2;
+
+        int n1 = amount;
+
+        System.out.println("500 = " + n500);
+        System.out.println("200 = " + n200);
+        System.out.println("100 = " + n100);
+        System.out.println("50 = " + n50);
+        System.out.println("20 = " + n20);
+        System.out.println("10 = " + n10);
+        System.out.println("5 = " + n5);
+        System.out.println("2 = " + n2);
+        System.out.println("1 = " + n1);
+
+        sc.close();
 
     }
 }
