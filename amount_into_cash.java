2360c6a71e6c8198e0943dd07fa0f65ca4691143
SIDDHARTH SINHA <siddharthsinha670@gmail.com>
2026-06-01 20:45:54 +0530

    Add amount_into_cash.java skeleton
    Create new Java file amount_into_cash.java for the 'Input Amount and Break Into Notes' task. Adds class and main method with a Scanner initialized; implementation to break amount into notes is not yet implemented.

---
+++ amount_into_cash.java
@@ -0,0 +1,10 @@
+//7. Input Amount and Break Into Notes
+
+import java.util.Scanner;
+
+class amount_into_cash {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+    }
+}
