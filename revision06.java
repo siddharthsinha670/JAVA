88cd0ec46e8a375d3638c4b96030da881d98b0d6
SIDDHARTH SINHA <siddharthsinha670@gmail.com>
2026-07-03 14:07:02 +0530

    Add revision06 factorial program
    Add Java program (revision06.java) to find the factorial of a number, along with compiled class file.

---
+++ revision06.java
@@ -0,0 +1,27 @@
+//Write a program to find the factorial of a number.
+
+import java.util.Scanner;
+public class revision06
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner (System.in);
+        try
+        {
+            int fact , i;
+            System.out.println("Enter the integer number: ");
+            i = sc.nextInt();
+            fact = fact * i;
+            for(int i = 1; i <= n; i++)
+            {
+                System.out.println();
+            }
+
+        }
+        catch(Exception e)
+        {
+            System.out.println("please!, enter the integer value.");
+        }
+    }
+    
+}
